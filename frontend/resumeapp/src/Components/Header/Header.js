3c59b4ee1de30eb6d0dca8be940ce98700a8bba3
kwonjuyeong <20161527@vision.hoseo.edu>
2024-03-10 18:54:03 +0900

    리스트

--- frontend/resumeapp/src/Components/Header/Header.js
+++ frontend/resumeapp/src/Components/Header/Header.js
@@ -10,7 +10,19 @@ function Header() {
       </div>
       <nav>
         <ul className="menu-group">
-          <li><a href="#job-info">채용정보</a></li>
+          <li><a href="#job-info">채용정보</a>
+          
+          {isSubMenuOpen && (
+              <ul className="sub-menu">
+                <li><a href="#job-postings">채용공고</a></li>
+                <li><a href="#job-description">직무소개</a></li>
+                {/* 이하 하위 메뉴 항목들 추가 */}
+              </ul>
+            )}
+
+<ul class="subMenu " style="opacity: 0; width: 0px; display: block; overflow: hidden;">		<li class="">			<a href="/app/jobnotice/list">채용공고</a>		</li>		<li class="">			<a href="http://www.emro.co.kr/2017/kr/duty.html" target="_blank"> 직무소개</a>		</li>		<li class="">		<span>채용문의</span>		<ul>		<li class=""><a href="/bbs/appsite/notice/list">공지사항</a></li>		<li class=""><a href="/bbs/appsite/qna/write">채용 QnA</a></li>		</ul>		</li>		</ul>
+          
+          </li>
           <li><a href="#apply">입사지원</a></li>
           <li><a href="#company">회사소개</a></li>
           <li><a href="#hr-system">인사제도</a></li>
